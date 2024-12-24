f66dfde80a2969ac5e9ebd218869ce00818b3e11
Gokukhan1 <Gokukhan1@gmail.com>
2024-12-24 10:17:54 +0000

    Auto Fixes

--- FallenRobot/utils/pastebin.py
+++ FallenRobot/utils/pastebin.py
@@ -1,6 +1,7 @@
 import socket
 from asyncio import get_running_loop
 from functools import partial
+
 import aiohttp
 
 
@@ -21,6 +22,7 @@ async def paste(content):
     loop = get_running_loop()
     link = await loop.run_in_executor(None, partial(_netcat, "ezup.dev", 9999, content))
     return link
+
 
 BASE = "https://batbin.me/"
 
